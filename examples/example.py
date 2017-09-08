a26071f7f461be8051b3345bb31d13f4c4502637
Simon Perkins <simon.perkins@gmail.com>
2017-09-08 15:18:54 +0200

    Use xarray ufuncs and Dataset.assign
    Utilise the correct xarray interface.

--- examples/example.py
+++ examples/example.py
@@ -3,6 +3,7 @@ from contextlib import contextmanager
 import time
 
 import dask.array as da
+import xarray.ufuncs as xru
 import xarray_ms
 
 def create_parser():
@@ -33,22 +34,28 @@ def scheduler_context(args):
 args = create_parser().parse_args()
 
 with scheduler_context(args):
+    start = time.clock()
+
+    # Create dataset from Measurement Set
     ds = xarray_ms.xds_from_ms(args.ms, chunks=100000)
 
-    start = time.clock()
-    inv_flag = da.logical_not(ds.flag.data)
+    # Create dataset with flag inverted
+    ds = ds.assign(flag=xru.logical_not(ds.flag))
+    assert isinstance(ds.flag.data, da.Array)
 
-    ds['flag'] = (ds.flag.dims, inv_flag)
-
+    # Write the flag column to the Measurement Set
     xarray_ms.xds_to_table(ds, "FLAG").compute()
 
-    print "Time handling '%s'" % (time.clock() - start)
+    print "Flag inversion time '%s'" % (time.clock() - start)
 
-    print ds
-
-
-    # Load the ANTENNA table 
-    ads = xarray_ms.xds_from_table('::'.join((args.ms, "ANTENNA")),
+    #Load the ANTENNA table
+    ant_ds = xarray_ms.xds_from_table('::'.join((args.ms, "ANTENNA")),
                                     chunks=100000, table_schema="ANTENNA")
 
-    print ads
+    spw_ds = xarray_ms.xds_from_table('::'.join((args.ms, "SPECTRAL_WINDOW")),
+                                    chunks=100000, table_schema="SPECTRAL_WINDOW")
+
+    print ant_ds
+    print spw_ds
+    print ds
+
