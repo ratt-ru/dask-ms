9bc30bd3111563eb926e9e5a16167fc383acdb22
Simon Perkins <simon.perkins@gmail.com>
2018-03-27 10:07:23 +0200

    Use sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ MOCK_MODULES = ['dask', 'dask.array', 'numpy',
                 'pyrap', 'pyrap.tables', 'xarray']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
+import sphinx_rtd_theme
 import xarrayms
 
 # -- General configuration ---------------------------------------------
@@ -103,7 +104,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
