b00075694661cb0b9a6dda5dc47f996e0667521d
Simon Perkins <simon.perkins@gmail.com>
2018-05-08 16:25:12 +0200

    Provide an example illustrating joins with MS subtables (#5)

--- examples/example.py
+++ examples/example.py
@@ -1,77 +1,10 @@
 import argparse
-import logging
-from contextlib import contextmanager
-
 import dask
-
 from xarrayms import xds_from_table, xds_from_ms, xds_to_table, TableProxy
 
-@contextmanager
-def scheduler_context(args):
-    """ Set the scheduler to use, based on the script arguments """
-
-    import dask
-
-    sched_info = {}
-
-    try:
-        if args.scheduler in ("mt", "thread", "threaded", "threading"):
-            import dask.threaded
-            logging.info("Using multithreaded scheduler")
-            dask.set_options(get=dask.threaded.get)
-            sched_info = {"type": "threaded"}
-        elif args.scheduler in ("mp", "multiprocessing"):
-            import dask.multiprocessing
-            logging.info("Using multiprocessing scheduler")
-            dask.set_options(get=dask.multiprocessing.get)
-            sched_info = {"type": "multiprocessing"}
-        else:
-            import distributed
-            local_cluster = None
-
-            if args.scheduler == "local":
-                local_cluster = distributed.LocalCluster(processes=False)
-                address = local_cluster.scheduler_address
-            elif args.scheduler.startswith('tcp'):
-                address = args.scheduler
-            else:
-                import json
-
-                with open(args.scheduler, 'r') as f:
-                    address = json.load(f)['address']
-
-            logging.info("Using distributed scheduler "
-                         "with address '{}'".format(address))
-            client = distributed.Client(address)
-            dask.set_options(get=client.get)
-            client.restart()
-            sched_info = {
-                "type" : "distributed",
-                "client" : client,
-                "local_cluster" : local_cluster }
-
-        yield
-    except Exception:
-        logging.exception("Error setting up scheduler", exc_info=True)
-
-    finally:
-        if sched_info["type"] == "distributed":
-            try:
-                client = sched_info["client"]
-            except KeyError:
-                pass
-            else:
-                client.close()
-
-            try:
-                local_cluster = sched_info["local_cluster"]
-            except KeyError:
-                pass
-            else:
-                local_cluster.close()
+from sched_context import scheduler_context
 
 if __name__ == "__main__":
-
     from dask.diagnostics import Profiler, ProgressBar
 
     def create_parser():
@@ -84,8 +17,6 @@ if __name__ == "__main__":
     args = create_parser().parse_args()
 
     with scheduler_context(args):
-
-
         # Create a dataset representing the entire antenna table
         ant_table =  '::'.join((args.ms, 'ANTENNA'))
 
