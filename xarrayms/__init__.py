68ab150db3dc13b62142efc69a291f9dda9c52fe
Simon Perkins <simon.perkins@gmail.com>
2018-05-28 10:00:16 +0200

    python 3 import compatibility

--- xarrayms/__init__.py
+++ xarrayms/__init__.py
@@ -1,7 +1,10 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 __author__ = """Simon Perkins"""
 __email__ = 'sperkins@ska.ac.za'
 __version__ = '0.1.0'
-
 
 def get_logger():
     import logging
