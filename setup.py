9efaf67f05950baa74d469e76c9f5ab82b5a742b
Simon Perkins <simon.perkins@gmail.com>
2018-06-01 14:24:05 +0200

    Fix quotes in setup.py so that bumpversion works

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def readme():
 
 
 setup(name='xarray-ms',
-      version="0.1.0",
+      version='0.1.0',
       description='xarray Datasets from CASA Tables.',
       long_description=readme(),
       url='http://github.com/ska-sa/xarray-ms',
