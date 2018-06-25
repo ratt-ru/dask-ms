e586a17a12d88b727332a8c1dccc0e4868643c7c
Simon Perkins <simon.perkins@gmail.com>
2018-06-25 14:11:19 +0200

    Test fragmentation handling

--- xarrayms/tests/test_ms.py
+++ xarrayms/tests/test_ms.py
@@ -6,6 +6,7 @@ import shutil
 import os
 
 import dask.array as da
+from mock import patch
 import numpy as np
 import pyrap.tables as pt
 import pytest
@@ -136,3 +137,111 @@ def test_ms_write(ms, group_cols, index_cols):
     # Check that state has been correctly written
     for i, (ds, expected) in enumerate(zip(xds, written_states)):
         assert np.all(ds.STATE_ID.data.compute() == expected)
+
+@pytest.mark.parametrize('group_cols', [
+    ["DATA_DESC_ID"]])
+@pytest.mark.parametrize('index_cols', [
+    ["TIME"]])
+def test_fragmented_ms(ms, group_cols, index_cols):
+    select_cols = index_cols + ["STATE_ID"]
+
+    # Zero everything to be sure
+    with pt.table(ms, readonly=False) as table:
+        table.putcol("STATE_ID", np.full(table.nrows(), 0, dtype=np.int32))
+
+    # Patch the get_row_runs function to check that it is called
+    # and resorting is invoked
+    # Unfragmented is 1.00, induce
+    # fragmentation handling
+    min_frag_level = 0.9999
+    side_effect_called = {'value': False }
+    from xarrayms.xarray_ms import get_row_runs
+    patch_target = "xarrayms.xarray_ms.get_row_runs"
+
+    def mock_row_runs(*args, **kwargs):
+        """ Calls get_row_runs and does some testing """
+        row_runs, row_resorts = get_row_runs(*args, **kwargs)
+        # Do some checks to ensure that fragmentation was handled
+        assert kwargs['min_frag_level'] == min_frag_level
+        assert all(isinstance(resort, np.ndarray) for resort in row_resorts)
+        side_effect_called['value'] = True
+        return row_runs, row_resorts
+
+
+    with patch(patch_target, side_effect=mock_row_runs):
+        xds = list(xds_from_ms(ms, columns=select_cols,
+                                group_cols=group_cols,
+                                index_cols=index_cols,
+                                min_frag_level=min_frag_level,
+                                chunks={"row": 1e9}))
+
+    # Check that mock_row_runs was called
+    assert side_effect_called['value'] == True
+
+    order = orderby_clause(index_cols)
+    written_states = []
+
+    for i, ds in enumerate(xds):
+        where = where_clause(group_cols, [getattr(ds, c) for c in group_cols])
+        query = "SELECT * FROM $ms %s %s" % (where, order)
+
+        # Check that each column is correctly read
+        with pt.taql(query) as Q:
+            for c in select_cols:
+                np_data = Q.getcol(c)
+                dask_data = getattr(ds, c).data.compute()
+                assert np.all(np_data == dask_data)
+
+        # Now write some data to the STATE_ID column
+        state = da.arange(i, i + ds.dims['row'], chunks=ds.chunks['row'])
+        written_states.append(state)
+        state = xr.DataArray(state, dims=['row'])
+        nds = ds.assign(STATE_ID=state)
+
+        side_effect_called = {'value': False}
+
+        with patch(patch_target, side_effect=mock_row_runs):
+            xds_to_table(nds, ms, "STATE_ID",
+                         min_frag_level=min_frag_level).compute()
+
+        assert side_effect_called['value'] == True
+
+
+    # Check that state has been correctly written
+    xds = list(xds_from_ms(ms, columns=select_cols,
+                            group_cols=group_cols,
+                            index_cols=index_cols,
+                            min_frag_level=min_frag_level,
+                            chunks={"row": 1e9}))
+
+    for i, (ds, expected) in enumerate(zip(xds, written_states)):
+        assert np.all(ds.STATE_ID.data.compute() == expected)
+
+
+@pytest.mark.parametrize('group_cols', [
+    ["DATA_DESC_ID"]])
+@pytest.mark.parametrize('index_cols', [
+    ["TIME"]])
+def test_unfragmented_ms(ms, group_cols, index_cols):
+    side_effect_called = {'value': False }
+    from xarrayms.xarray_ms import get_row_runs
+    patch_target = "xarrayms.xarray_ms.get_row_runs"
+
+    def mock_row_runs(*args, **kwargs):
+        """ Calls get_row_runs and does some testing """
+        row_runs, row_resorts = get_row_runs(*args, **kwargs)
+        # Do some checks to ensure that fragmentation was handled
+        assert kwargs['min_frag_level'] == False
+        assert all(resort is None for resort in row_resorts)
+        side_effect_called['value'] = True
+        return row_runs, row_resorts
+
+
+    with patch(patch_target, side_effect=mock_row_runs):
+        xds = list(xds_from_ms(ms, columns=index_cols,
+                                group_cols=group_cols,
+                                index_cols=index_cols,
+                                min_frag_level=False,
+                                chunks={"row": 1e9}))
+
+    assert side_effect_called['value'] is True
