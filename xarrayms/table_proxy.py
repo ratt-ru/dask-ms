3ead7d11f641128b9746205861d68ec5baae9bbe
Simon Perkins <simon.perkins@gmail.com>
2018-07-23 13:20:33 +0200

    Try write-locking only if function starts with put

--- xarrayms/table_proxy.py
+++ xarrayms/table_proxy.py
@@ -65,7 +65,7 @@ class TableProxy(object):
         try:
             # Acquire a lock and call the function
             if fn_requires_lock:
-                self._table.lock(write=self._write_lock)
+                self._table.lock(write=fn.startswith("put"))
 
             return getattr(self._table, fn)(*args, **kwargs)
 
