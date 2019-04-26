1d2b9c07bfc250d5358eb5f79828330caa3155e7
Simon Perkins <simon.perkins@gmail.com>
2019-04-26 19:25:58 +0200

    Write a data column

--- xarrayms/tests/conftest.py
+++ xarrayms/tests/conftest.py
@@ -1,6 +1,7 @@
 import shutil
 import os
 
+import numpy as np
 import pyrap.tables as pt
 import pytest
 
@@ -36,6 +37,10 @@ def ms(tmpdir_factory):
     # Column we'll write to
     state = [0,   0,   0,   0,   0,   0,   0,   0,   0,   0]
 
+    data_shape = (len(state), 16, 4)
+
+    data = np.random.random(data_shape) + np.random.random(data_shape)*1j
+
     # Create the table
     with pt.taql(create_table_query) as ms:
         ms.putcol("TIME", time)
@@ -45,6 +50,7 @@ def ms(tmpdir_factory):
         ms.putcol("ANTENNA2", ant2)
         ms.putcol("SCAN_NUMBER", scan)
         ms.putcol("STATE_ID", state)
+        ms.putcol("DATA", data)
 
     yield fn
 
