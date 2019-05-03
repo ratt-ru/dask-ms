94d3c0324af99dcc6e343e07540275170e65376e
Simon Perkins <simon.perkins@gmail.com>
2019-05-03 11:28:52 +0200

    flake8 fix

--- xarrayms/tests/test_table_cache.py
+++ xarrayms/tests/test_table_cache.py
@@ -2,8 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import pyrap.tables as pt
-
 from xarrayms.table_cache import TableCache, TableWrapper, MismatchedLocks
 import pytest
 
