51bbff8865ecbf6d64906ea5297d998277f1ea39
Simon Perkins <simon.perkins@gmail.com>
2019-05-03 16:09:03 +0200

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def readme():
 
 
 setup(name='xarray-ms',
-      version='0.1.3',
+      version='0.1.4',
       description='xarray Datasets from CASA Tables.',
       long_description=readme(),
       url='http://github.com/ska-sa/xarray-ms',
--- xarrayms/__init__.py
+++ xarrayms/__init__.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 
 __author__ = """Simon Perkins"""
 __email__ = 'sperkins@ska.ac.za'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 from xarrayms.xarray_ms import (xds_from_table,             # noqa
                                 xds_to_table, xds_from_ms)  # noqa
