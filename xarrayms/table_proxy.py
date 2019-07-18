69738b6fa18aee1e0af43e7deb67110051e298c4
Simon Perkins <simon.perkins@gmail.com>
2019-07-18 11:30:44 +0200

    Simplify creating delete weakrefs

--- xarrayms/table_proxy.py
+++ xarrayms/table_proxy.py
@@ -27,21 +27,23 @@ class ExecutorMetaClass(type):
                 return instance
 
 
+def executor_delete_reference(ex, threadpool_executor):
+    # http://pydev.blogspot.com/2015/01/creating-safe-cyclic-reference.html
+    # To avoid cyclic references, self may not be used within _callback
+    def _callback(ref):
+        try:
+            threadpool_executor.shutdown(wait=True)
+        except Exception:
+            log.exception("Error shutting down executor in _callback")
+
+    return weakref.ref(ex, _callback)
+
+
 class Executor(ExecutorMetaClass("base", (object,), {})):
     def __init__(self):
         # Initialise a single thread
         self.ex = ex = cf.ThreadPoolExecutor(1)
-
-        # http://pydev.blogspot.com/2015/01/creating-safe-cyclic-reference.html
-        def _callback(ref):
-            # to avoid cyclic references, self may
-            # not be used within this function
-            try:
-                ex.shutdown(wait=True)
-            except Exception:
-                log.exception("Error shutting down executor in _callback")
-
-        self.__del_ref = weakref.ref(self, _callback)
+        self.__del_ref = executor_delete_reference(self, ex)
 
     def submit(self, *args, **kwargs):
         return self.ex.submit(*args, **kwargs)
@@ -50,7 +52,7 @@ class Executor(ExecutorMetaClass("base", (object,), {})):
         return self.ex.shutdown(*args, **kwargs)
 
     def __reduce__(self):
-        return (Executor, ())  # No args/kwargs, they're ignored in __init__
+        return (Executor, ())
 
 
 _table_cache = weakref.WeakValueDictionary()
@@ -70,6 +72,18 @@ class TableProxyMetaClass(type):
                 return instance
 
 
+def proxy_delete_reference(table_proxy, ex, table):
+    # http://pydev.blogspot.com/2015/01/creating-safe-cyclic-reference.html
+    # To avoid cyclic references, self may not be used within _callback
+    def _callback(ref):
+        try:
+            ex.submit(table.close).result()
+        except Exception:
+            log.exception("Error closing table in _callback")
+
+    return weakref.ref(table_proxy, _callback)
+
+
 def _map_create_proxy(cls, factory, args, kwargs):
     """ Support pickling of kwargs in TableProxy.__reduce__ """
     return cls(factory, *args, **kwargs)
@@ -82,17 +96,7 @@ class TableProxy(TableProxyMetaClass("base", (object,), {})):
         self._factory = factory
         self._args = args
         self._kwargs = kwargs
-
-        # http://pydev.blogspot.com/2015/01/creating-safe-cyclic-reference.html
-        def _callback(ref):
-            # to avoid cyclic references, self may
-            # not be used within this function
-            try:
-                ex.submit(table.close).result()
-            except Exception:
-                log.exception("Error closing table in _callback")
-
-        self.__del_ref = weakref.ref(self, _callback)
+        self.__del_ref = proxy_delete_reference(self, ex, table)
 
     def __reduce__(self):
         """ Defer to _map_create_proxy to support kwarg pickling """
