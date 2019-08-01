9c5cfbca56da98035ed54e08884da27df7f64e3b
Simon Perkins <simon.perkins@gmail.com>
2019-08-01 20:35:21 +0200

    Add stress testing capabilities

--- conftest.py
+++ conftest.py
@@ -1,6 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 from os.path import join as pjoin
 
 collect_ignore = ["setup.py"]
-collect_ignore += [pjoin('xarrayms', 'tests', 'test_correct_read.py'),
-                   pjoin('xarrayms', 'tests', 'test_correct_write.py'),
-                   pjoin('xarrayms', 'tests', 'test_zarr_write.py')]
+
+
+def pytest_addoption(parser):
+    parser.addoption('--stress', action='store_true', dest="stress",
+                     default=False, help="Enable stress tests")
+
+
+def pytest_configure(config):
+    if not config.option.stress:
+        setattr(config.option, 'markexpr', 'not stress')
---
+++ xarrayms/tests/test_stress.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import pytest
+
+
+@pytest.mark.stress
+def test_stress():
+    pass
