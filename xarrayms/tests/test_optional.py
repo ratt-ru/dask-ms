3eccd3b733c927c83f181d0f61690f6d6ef9526e
Simon Perkins <simon.perkins@gmail.com>
2019-08-13 17:59:32 +0200

    Expand range of optional tests

--- xarrayms/tests/test_optional.py
+++ xarrayms/tests/test_optional.py
@@ -26,7 +26,7 @@ from xarrayms.columns import infer_casa_type
 @pytest.mark.parametrize("column", ["BAZ"])
 @pytest.mark.parametrize("dtype", [np.int32])
 def test_variable_column_dimensions(tmp_path, column, dtype):
-    """ Exists to illustrate how just variably shaped CASA columns can be """
+    """ ndim set to -1, we can put anything in the column """
     casa_type = infer_casa_type(dtype)
 
     # Column descriptor
@@ -62,7 +62,7 @@ def test_variable_column_dimensions(tmp_path, column, dtype):
 @pytest.mark.parametrize("column", ["BAZ"])
 @pytest.mark.parametrize("dtype", [np.int32])
 def test_variable_column_shapes(tmp_path, column, dtype):
-    """ Exists to illustrate how just variably shaped CASA columns can be """
+    """ ndim set to 2, but shapes are variable """
     casa_type = infer_casa_type(dtype)
 
     # Column descriptor
@@ -103,7 +103,7 @@ def test_variable_column_shapes(tmp_path, column, dtype):
 @pytest.mark.parametrize("column", ["BAZ"])
 @pytest.mark.parametrize("dtype", [np.int32])
 def test_fixed_column_shapes(tmp_path, column, dtype):
-    """ Exists to illustrate how just variably shaped CASA columns can be """
+    """ Fixed column, shape and ndim must be supplied """
     casa_type = infer_casa_type(dtype)
 
     # Column descriptor
@@ -112,7 +112,6 @@ def test_fixed_column_shapes(tmp_path, column, dtype):
                      'dataManagerGroup': '',
                      'dataManagerType': '',
                      'keywords': {},
-                     # ndim can be left out, but wierd things happen
                      'ndim': 2,
                      'shape': (20, 30),
                      'maxlen': 0,
@@ -137,3 +136,71 @@ def test_fixed_column_shapes(tmp_path, column, dtype):
         with pytest.raises(RuntimeError, match=err_str):
             T.putcol(column, np.zeros((5, 40, 30), dtype=dtype),
                      startrow=0, nrow=5)
+
+
+@pytest.mark.optional
+@pytest.mark.parametrize("column", ["BAZ"])
+@pytest.mark.parametrize("dtype", [np.int32])
+def test_only_row_shape(tmp_path, column, dtype):
+    """ Missing ndim implies row only! """
+    casa_type = infer_casa_type(dtype)
+
+    # Column descriptor
+    desc = {'desc': {'_c_order': True,
+                     'comment': '%s column' % column,
+                     'dataManagerGroup': '',
+                     'dataManagerType': '',
+                     'keywords': {},
+                     'maxlen': 0,
+                     'option': 0,
+                     'valueType': casa_type},
+            'name': column}
+
+    table_desc = pt.maketabdesc([desc])
+    fn = os.path.join(str(tmp_path), "test.table")
+
+    with pt.table(fn, table_desc, nrow=10, ack=False) as T:
+        # Put 10 rows into the table
+        T.putcol(column, np.zeros(10, dtype=dtype))
+        assert T.getcol(column).shape == (10,)
+
+        # Must be ndim == 2
+        err_str = 'Vector<T>: ndim of other array > 1 ndim 1 differs from 2'
+        with pytest.raises(RuntimeError, match=err_str):
+            T.putcol(column, np.zeros((5, 40), dtype=dtype))
+
+        # shape != (20, 30)
+        err_str = 'Vector<T>: ndim of other array > 1 ndim 1 differs from 3'
+        with pytest.raises(RuntimeError, match=err_str):
+            T.putcol(column, np.zeros((5, 40, 30), dtype=dtype),
+                     startrow=0, nrow=5)
+
+
+@pytest.mark.optional
+@pytest.mark.parametrize("column", ["BAZ"])
+@pytest.mark.parametrize("dtype", [np.int32])
+def test_scalar_ndim(tmp_path, column, dtype):
+    """ ndim set to zero implies scalars """
+    casa_type = infer_casa_type(dtype)
+
+    # Column descriptor
+    desc = {'desc': {'_c_order': True,
+                     'comment': '%s column' % column,
+                     'dataManagerGroup': '',
+                     'dataManagerType': '',
+                     'keywords': {},
+                     'maxlen': 0,
+                     'ndim': 0,
+                     'option': 0,
+                     'valueType': casa_type},
+            'name': column}
+
+    table_desc = pt.maketabdesc([desc])
+    fn = os.path.join(str(tmp_path), "test.table")
+
+    with pt.table(fn, table_desc, nrow=10, ack=False) as T:
+        for r in range(10):
+            T.putcell(column, r, r)
+
+        for r in range(10):
+            assert T.getcell(column, r) == r
