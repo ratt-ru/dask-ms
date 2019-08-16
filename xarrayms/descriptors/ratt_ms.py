8500cf6757d611d56cc191488f9a4b1d6260ae14
Simon Perkins <simon.perkins@gmail.com>
2019-08-16 16:29:36 +0200

    Greatly simplify

--- xarrayms/descriptors/ratt_ms.py
+++ xarrayms/descriptors/ratt_ms.py
@@ -12,37 +12,6 @@ from xarrayms.descriptors.ms import MSDescriptorBuilder
 
 @register_descriptor_builder("ratt_ms")
 class RATTMSDescriptorBuilder(MSDescriptorBuilder):
-    def default_descriptor(self):
-        desc = super(RATTMSDescriptorBuilder, self).default_descriptor()
-
-        desc['BITFLAG'] = {
-            '_c_order': True,
-            'comment': 'BITFLAG Column',
-            'dataManagerGroup': 'StandardStMan',
-            'dataManagerType': 'StandardStMan',
-            'keywords': {'UNIT': 'Jy'},
-            'maxlen': 0,
-            'ndim': 2,  # (chan, corr)
-            'option': 0,
-            # comment this out to force type setting below
-            # 'valueType': 'BYTE'}
-        }
-
-        return desc
-
-    def descriptor(self, variables, default_desc):
-        # Override the default BITFLAG type
-        try:
-            bitflag = variables['BITFLAG']
-        except KeyError:
-            pass
-        else:
-            casa_type = infer_casa_type(bitflag[0].dtype)
-            default_desc['BITFLAG']['valueType'] = casa_type
-
-        return super(RATTMSDescriptorBuilder, self).descriptor(variables,
-                                                               default_desc)
-
     def fix_columns(self, variables, desc, dim_sizes):
         desc = super(RATTMSDescriptorBuilder, self).fix_columns(variables,
                                                                 desc,
