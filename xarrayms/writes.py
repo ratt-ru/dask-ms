8f5e989301dcbf86f246ae8c697da2d741fc7fa4
Simon Perkins <simon.perkins@gmail.com>
2019-08-19 22:36:06 +0200

    flake8

--- xarrayms/writes.py
+++ xarrayms/writes.py
@@ -7,7 +7,6 @@ from __future__ import print_function
 import logging
 
 import dask
-from dask.core import flatten
 import dask.array as da
 from dask.highlevelgraph import HighLevelGraph
 import numpy as np
