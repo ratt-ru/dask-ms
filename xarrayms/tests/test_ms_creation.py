2ee33c4e4f4d9e799c0c58b493146b3ebdedded6
Simon Perkins <simon.perkins@gmail.com>
2019-08-19 13:56:36 +0200

    Add an MS creation test

---
+++ xarrayms/tests/test_ms_creation.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from itertools import product
+from pprint import pprint
+
+import dask
+import dask.array as da
+import numpy as np
+from numpy.testing import assert_array_equal
+import pyrap.tables as pt
+import pytest
+
+from xarrayms import xds_to_table
+
+
+@pytest.mark.parametrize("chunks", [{
+    "row": (10,),
+}])
+@pytest.mark.parametrize("num_chans", [
+    [16, 32],
+])
+@pytest.mark.parametrize("corr_types", [
+    [[9, 10, 11, 12], [5, 8]],
+    [[5, 6, 7, 8], [9, 12]]
+])
+def test_ms_create(tmp_path, chunks, num_chans, corr_types):
+    # Set up
+    xr = pytest.importorskip('xarray')
+    rs = np.random.RandomState(42)
+
+    ms_path = tmp_path / "create.ms"
+    ms_table_name = str(ms_path)
+    ant_table_name = str(ms_path / "ANTENNA")
+    ddid_table_name = str(ms_path / "DATA_DESCRIPTION")
+    pol_table_name = str(ms_path / "POLARIZATION")
+    spw_table_name = str(ms_path / "SPECTRAL_WINDOW")
+
+    ms_datasets = []
+    ant_datasets = []
+    ddid_datasets = []
+    pol_datasets = []
+    spw_datasets = []
+
+    # For comparison
+    all_data_desc_id = []
+    all_data = []
+
+    # Create ANTENNA dataset of 64 antennas
+    # Each column in the ANTENNA has a fixed shape so we
+    # can represent all rows with one dataset
+    na = 64
+    position = da.random.random((na, 3))*10000
+    offset = da.random.random((na, 3))
+    names = np.array(['ANTENNA-%d' % i for i in range(na)], dtype=np.object)
+    ds = xr.Dataset({
+        'POSITION': xr.DataArray(position, dims=("row", "xyz")),
+        'OFFSET': xr.DataArray(offset, dims=("row", "xyz")),
+        'NAMES': xr.DataArray(da.from_array(names, chunks=na), dims=("row",)),
+    })
+    ant_datasets.append(ds)
+
+    # Create POLARISATION datasets.
+    # Dataset per output row required because column shapes are variable
+    for corr_type in corr_types:
+        dask_num_corr = da.full((1,), len(corr_type), dtype=np.int32)
+        dask_corr_type = da.from_array(corr_type,
+                                       chunks=len(corr_type))[None, :]
+        ds = xr.Dataset({
+            "NUM_CORR": xr.DataArray(dask_num_corr, dims=("row",)),
+            "CORR_TYPE": xr.DataArray(dask_corr_type, dims=("row", "corr"))
+        })
+
+        pol_datasets.append(ds)
+
+    # Create multiple MeerKAT L-band SPECTRAL_WINDOW datasets
+    # Dataset per output row required because column shapes are variable
+    for num_chan in num_chans:
+        dask_num_chan = da.full((1,), num_chan, dtype=np.int32)
+        dask_chan_freq = da.linspace(.856e9, 2*.856e9, num_chan,
+                                     chunks=num_chan)[None, :]
+        dask_chan_width = da.full((1, num_chan), .856e9/num_chan)
+
+        ds = xr.Dataset({
+            "NUM_CHAN": xr.DataArray(dask_num_chan, dims=("row",)),
+            "CHAN_FREQ": xr.DataArray(dask_chan_freq, dims=("row", "chan")),
+            "CHAN_WIDTH": xr.DataArray(dask_chan_width, dims=("row", "chan"))
+        })
+
+        spw_datasets.append(ds)
+
+    # For each cartesian product of SPECTRAL_WINDOW and POLARIZATION
+    # create a corresponding DATA_DESCRIPTION.
+    # Each column has fixed shape so we handle all rows at once
+    spw_ids, pol_ids = zip(*product(range(len(num_chans)),
+                                    range(len(corr_types))))
+    dask_spw_ids = da.asarray(np.asarray(spw_ids, dtype=np.int32))
+    dask_pol_ids = da.asarray(np.asarray(pol_ids, dtype=np.int32))
+    ddid_datasets.append(xr.Dataset({
+        "SPECTRAL_WINDOW_ID": xr.DataArray(dask_spw_ids, dims=("row",)),
+        "POLARIZATION_ID": xr.DataArray(dask_pol_ids, dims=("row",)),
+    }))
+
+    # Now create the associated MS dataset
+    for ddid, (spw_id, pol_id) in enumerate(zip(spw_ids, pol_ids)):
+        # Infer row, chan and correlation shape
+        row = sum(chunks['row'])
+        chan = spw_datasets[spw_id].CHAN_FREQ.shape[1]
+        corr = pol_datasets[pol_id].CORR_TYPE.shape[1]
+
+        # Create some dask vis data
+        dims = ("row", "chan", "corr")
+        np_data = (rs.normal(size=(row, chan, corr)) +
+                   1j*rs.normal(size=(row, chan, corr))).astype(np.complex64)
+
+        data_chunks = tuple((chunks['row'], chan, corr))
+        dask_data = da.from_array(np_data, chunks=data_chunks)
+        # Create dask ddid column
+        dask_ddid = da.full(row, ddid, chunks=chunks['row'], dtype=np.int32)
+        dataset = xr.Dataset({
+            'DATA': xr.DataArray(dask_data, dims=dims),
+            'DATA_DESC_ID': xr.DataArray(dask_ddid, dims=("row",))
+        })
+        ms_datasets.append(dataset)
+        all_data.append(dask_data)
+        all_data_desc_id.append(dask_ddid)
+
+    ms_writes = xds_to_table(ms_datasets, ms_table_name)
+    ant_writes = xds_to_table(ant_datasets, ant_table_name)
+    pol_writes = xds_to_table(pol_datasets, pol_table_name)
+    spw_writes = xds_to_table(spw_datasets, spw_table_name)
+    ddid_writes = xds_to_table(ddid_datasets, ddid_table_name)
+
+    dask.compute(ms_writes, ant_writes, pol_writes, spw_writes, ddid_writes)
+
+    # Check POLARIZATION table correctly created
+    with pt.table(pol_table_name, ack=False) as P:
+        for r, corr_type in enumerate(corr_types):
+            assert_array_equal(P.getcol("CORR_TYPE", startrow=r, nrow=1),
+                               [corr_type])
+            assert_array_equal(P.getcol("NUM_CORR", startrow=r, nrow=1),
+                               [len(corr_type)])
+
+        required_desc = pt.required_ms_desc("POLARIZATION")
+        required_columns = set(k for k in required_desc.keys()
+                               if not k.startswith("_"))
+
+        assert set(P.colnames()) == set(required_columns)
+
+    # Check SPECTRAL_WINDOW table correctly created
+    with pt.table(spw_table_name, ack=False) as S:
+        for r, num_chan in enumerate(num_chans):
+            assert_array_equal(S.getcol("NUM_CHAN", startrow=r, nrow=1)[0],
+                               num_chan)
+            assert_array_equal(S.getcol("CHAN_FREQ", startrow=r, nrow=1)[0],
+                               np.linspace(.856e9, 2*.856e9, num_chan))
+            assert_array_equal(S.getcol("CHAN_WIDTH", startrow=r, nrow=1)[0],
+                               np.full(num_chan, .856e9 / num_chan))
+
+        required_desc = pt.required_ms_desc("SPECTRAL_WINDOW")
+        required_columns = set(k for k in required_desc.keys()
+                               if not k.startswith("_"))
+
+        assert set(S.colnames()) == set(required_columns)
+
+    # We should get a cartesian product out
+    with pt.table(ddid_table_name, ack=False) as D:
+        spw_id, pol_id = zip(*product(range(len(num_chans)),
+                                      range(len(corr_types))))
+        assert_array_equal(pol_id, D.getcol("POLARIZATION_ID"))
+        assert_array_equal(spw_id, D.getcol("SPECTRAL_WINDOW_ID"))
+
+        required_desc = pt.required_ms_desc("DATA_DESCRIPTION")
+        required_columns = set(k for k in required_desc.keys()
+                               if not k.startswith("_"))
+
+        assert set(D.colnames()) == set(required_columns)
+
+    with pt.table(str(ms_path), ack=False) as T:
+        # DATA_DESC_ID's are all the same shape
+        assert_array_equal(T.getcol("DATA_DESC_ID"),
+                           da.concatenate(all_data_desc_id))
+
+        # DATA is variably shaped (on DATA_DESC_ID) so we
+        # compared each one separately.
+        for ddid, data in enumerate(all_data):
+            ms_data = T.getcol("DATA", startrow=ddid*row, nrow=row)
+            assert_array_equal(ms_data, data)
+
+        required_desc = pt.required_ms_desc()
+        required_columns = set(k for k in required_desc.keys()
+                               if not k.startswith("_"))
+
+        # Check we have the required columns
+        assert set(T.colnames()) == required_columns.union(["DATA",
+                                                            "DATA_DESC_ID"])
