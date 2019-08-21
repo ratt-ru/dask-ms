e6c3ba8aa4341253c29f95d784c6f907cd3229a1
Simon Perkins <simon.perkins@gmail.com>
2019-08-21 14:19:15 +0200

    Setup bumpversions for alphas and fix version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def readme():
 
 
 setup(name='dask-ms',
-      version='0.2.0',
+      version='0.2.0-alpha1',
       description='xarray Datasets from CASA Tables.',
       long_description=readme(),
       url='http://github.com/ska-sa/dask-ms',
