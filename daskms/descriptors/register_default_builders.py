962ede0da4c6393382dc9a7542aca5ff331336c9
Simon Perkins <simon.perkins@gmail.com>
2019-08-23 12:07:48 +0200

    flake8

--- daskms/descriptors/register_default_builders.py
+++ daskms/descriptors/register_default_builders.py
@@ -4,6 +4,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import daskms.descriptors.ms
-import daskms.descriptors.ms_subtable
-import daskms.descriptors.ratt_ms
+import daskms.descriptors.ms  # noqa
+import daskms.descriptors.ms_subtable  # noqa
+import daskms.descriptors.ratt_ms  # noqa
