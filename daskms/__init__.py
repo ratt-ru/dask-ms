e55325d34ba6e15def3e7d40482a8fabbd120f50
Simon Perkins <simon.perkins@gmail.com>
2019-09-10 15:27:48 +0200

    Bump version: 0.2.0-rc2 → 0.2.0-rc3

--- daskms/__init__.py
+++ daskms/__init__.py
@@ -8,7 +8,7 @@ import logging
 
 __author__ = """Simon Perkins"""
 __email__ = 'sperkins@ska.ac.za'
-__version__ = '0.2.0-rc2'
+__version__ = '0.2.0-rc3'
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
