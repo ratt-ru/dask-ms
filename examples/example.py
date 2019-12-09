c82cbf3f65cbc2793bf86473f6aa11cad0845e20
Simon Perkins <simon.perkins@gmail.com>
2019-12-09 15:42:00 +0200

    Fix examples

--- examples/example.py
+++ examples/example.py
@@ -1,6 +1,12 @@
 import argparse
+from contextlib import ExitStack
 import dask
 from daskms import xds_from_table, xds_from_ms, xds_to_table, TableProxy
+
+try:
+    import bokeh
+except ImportError:
+    bokeh = None
 
 from sched_context import scheduler_context
 
@@ -21,9 +27,9 @@ if __name__ == "__main__":
         ant_table = '::'.join((args.ms, 'ANTENNA'))
 
         for ant_ds in xds_from_table(ant_table):
-            print(ant_ds)
             print(dask.compute(ant_ds.NAME.data,
-                               ant_ds.POSITION.data, ant_ds.DISH_DIAMETER.data))
+                               ant_ds.POSITION.data,
+                               ant_ds.DISH_DIAMETER.data))
 
         # Create datasets representing each row of the spw table
         spw_table = '::'.join((args.ms, 'SPECTRAL_WINDOW'))
@@ -36,13 +42,26 @@ if __name__ == "__main__":
         # Create datasets from a partioning of the MS
         datasets = list(xds_from_ms(args.ms, chunks={'row': args.chunks}))
 
+        writes = []
+
         for ds in datasets:
             print(ds)
 
             # Try write the STATE_ID column back
             write = xds_to_table(ds, args.ms, 'STATE_ID')
-            with ProgressBar(), Profiler() as prof:
-                write.compute()
+            writes.append(write)
 
-            # Profile
+        with ExitStack() as stack:
+            stack.enter_context(ProgressBar())
+
+            if bokeh is not None:
+                prof = Profiler()
+                stack.enter_context(prof)
+            else:
+                prof = None
+
+            dask.compute(writes)
+
+        # Profile
+        if prof:
             prof.visualize(file_path="chunked.html")
