5bdca7bedb4f222969178eb55d0461f288d6755f
Simon Perkins <simon.perkins@gmail.com>
2020-04-24 16:26:14 +0200

    Bump version: 0.2.3 → 0.2.4

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def readme():
 
 
 setup(name='dask-ms',
-      version='0.2.3',
+      version='0.2.4',
       description='xarray Datasets from CASA Tables.',
       long_description=readme(),
       url='http://github.com/ska-sa/dask-ms',
