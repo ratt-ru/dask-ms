7575947c6ac121e536846fc1d7531523de09d750
Simon Perkins <simon.perkins@gmail.com>
2020-10-28 13:53:04 +0200

    Data Column Expressions

---
+++ daskms/expressions.py
@@ -0,0 +1,98 @@
+import ast
+
+import dask.array as da
+
+operators = {
+    ast.Mult: da.multiply,
+    ast.Div: da.divide,
+    ast.Add: da.add,
+    ast.Sub: da.subtract,
+    ast.USub: da.negative
+}
+
+
+class Visitor(ast.NodeTransformer):
+    def __init__(self, datasets):
+        self.datasets = datasets
+
+    def visit_Constant(self, node):
+        pass
+
+    def visit_Num(self, node):
+        return node.n
+
+    def visit_BinOp(self, node):
+        try:
+            op = operators[type(node.op)]
+        except KeyError:
+            raise ValueError(f"Unsupported operator {type(node.op)}")
+
+        left = self.visit(node.left)
+        right = self.visit(node.right)
+        rtype = type(right)
+        ltype = type(left)
+
+        if ltype is list and rtype is list:
+            assert len(left) == len(right)
+            return [op(l, r) for l, r in zip(left, right)]
+        elif ltype is list:
+            return [op(l, right) for l in left]
+        elif rtype is list:
+            return [op(left, r) for r in right]
+        else:
+            return op(left, right)
+
+    def visit_Name(self, node):
+        columns = []
+
+        for i, ds in enumerate(self.datasets):
+            xdarray = getattr(ds, node.id)
+
+            try:
+                dims = xdarray.dims
+            except AttributeError:
+                raise TypeError(f"{type(xdarray)} does not look "
+                                f"like a valid Dataset Array")
+            else:
+                if dims != ("row", "chan", "corr"):
+                    raise ValueError(f"{xarray} does not look "
+                                     f"like a valid DATA array. "
+                                     f"Should have (row, chan, corr) dims"
+                                     f"Instead has {dims}")
+
+            columns.append(xdarray.data)
+
+        return columns
+
+
+def expr(expression, datasets):
+    if isinstance(datasets, (list, tuple)):
+        promoted_datasets = list(datasets)
+    else:
+        promoted_datasets = [datasets]
+
+    mod = ast.parse(expression)
+    assert isinstance(mod, ast.Module)
+
+    if len(mod.body) != 1:
+        raise ValueError("Single Expression Only")
+
+    expr = mod.body[0]
+
+    if not isinstance(expr, ast.Expr):
+        raise ValueError("Single Expression Only")
+
+    v = Visitor(promoted_datasets)
+    node = v.visit(expr)
+
+    new_datasets = []
+
+    for i, ds in enumerate(datasets):
+        nds = ds.assign(DATA_EXPRESSION=(("row", "chan", "corr"),
+                                         node.value[i]))
+        new_datasets.append(nds)
+
+    if not isinstance(promoted_datasets, (tuple, list)):
+        return new_datasets[0]
+
+    return new_datasets
---
+++ daskms/tests/test_expression.py
@@ -0,0 +1,22 @@
+from daskms import xds_from_ms
+from daskms.expressions import expr
+
+from numpy.testing import assert_array_equal
+
+
+def test_expressions(ms):
+    datasets = xds_from_ms(ms)
+
+    for i, ds in enumerate(datasets):
+        datasets[i] = ds.assign(DIR1_DATA=ds.DATA, DIR2_DATA=ds.DATA, DIR3_DATA=ds.DATA)
+
+    results = [
+        ds.DATA.data / (ds.DIR1_DATA.data + ds.DIR2_DATA.data + ds.DIR3_DATA.data) * 4
+        for ds in datasets
+    ]
+
+    string = "DATA / (DIR1_DATA + DIR2_DATA + DIR3_DATA)*4"
+    datasets = expr(string, datasets)
+
+    for i, ds in enumerate(datasets):
+        assert_array_equal(results[i], ds.DATA_EXPRESSION.data)
