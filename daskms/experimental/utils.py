71e4896286b8e83dd6bf7181330aa07f19efb387
Simon Perkins <simon.perkins@gmail.com>
2021-05-13 11:50:04 +0200

    Fix zarr coordinate writes

--- daskms/experimental/utils.py
+++ daskms/experimental/utils.py
@@ -1,9 +1,13 @@
+import logging
 from pathlib import Path
 
 import dask
 import dask.array as da
 from dask.highlevelgraph import HighLevelGraph
 import numpy as np
+
+
+log = logging.getLogger(__name__)
 
 
 def extent_args(dims, chunks):
@@ -31,14 +35,8 @@ def column_iterator(variables, columns):
     if columns is None or columns == "ALL":
         return variables.items()
     else:
-        column_set = set(columns)
-        unknown_columns = column_set - set(variables.keys())
-
-        if len(unknown_columns) > 0:
-            raise ValueError(f"{unknown_columns} are not present "
-                             f"in the dataset.")
-
-        return ((k, variables[k]) for k in column_set)
+        columns = set(columns) & set(variables.keys(0))
+        return {c: variables[c] for c in columns}
 
 
 def promote_columns(columns):
@@ -54,6 +52,51 @@ def promote_columns(columns):
     else:
         raise TypeError(f"'columns' must be None or str "
                         f"or list of str. Got {columns}")
+
+
+def select_vars_and_coords(dataset, columns):
+    """
+    Returns
+    -------
+    data_vars : dict
+        Dictionary of data variables
+    coords : dict
+        Dictionary of coordinates
+    """
+    if columns is None or columns == "ALL":
+        # Return all data variables and coordinates
+        ret_data_vars = dict(dataset.data_vars)
+        ret_coords = dict(dataset.coords)
+    else:
+        # Use specified variables and coordinates
+        # BUT, we also include any coordinates
+        # that variables and other coordinates depend on
+        column_set = set(columns)
+        data_vars = dataset.data_vars
+        coords = dataset.coords
+        data_cols = column_set & set(data_vars.keys())
+        coord_cols = column_set & set(coords.keys())
+
+        for c in coord_cols:
+            coord_cols.union(*(d for d in coords[c].dims))
+
+        ret_data_vars = {}
+
+        for c in data_cols:
+            ret_data_vars[c] = v = data_vars[c]
+            coord_cols.union(*(d for d in v.dims))
+
+        ret_coords = {}
+
+        for c in coord_cols:
+            try:
+                v = coords[c]
+            except KeyError:
+                continue
+            else:
+                ret_coords[c] = v
+
+    return ret_data_vars, ret_coords
 
 
 def store_path_split(store):
