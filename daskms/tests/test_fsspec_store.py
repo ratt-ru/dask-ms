2d6620971fc4ed6cad279d22d25985808424460f
Simon Perkins <simon.perkins@gmail.com>
2022-01-14 10:50:19 +0200

    [skip ci] Pass FSMap as Zarr store

--- daskms/tests/test_fsspec_store.py
+++ daskms/tests/test_fsspec_store.py
@@ -1,10 +1,21 @@
+import numpy as np
+import pytest
+
 from daskms.fsspec_store import Store
 
 def test_local_store(tmp_path):
+    zarr = pytest.importorskip("zarr")
     payload = "How now brown cow"
     filename = "cow.txt"
-    dummy = tmp_path / filename
-    dummy.write_text(payload)
+    (tmp_path / filename).write_text(payload)
+    (tmp_path / "foo.txt").write_text(payload)
+    (tmp_path / "bar.txt").write_text(payload)
+    (tmp_path / "qux.txt").write_text(payload)
     store = Store(str(tmp_path))
 
     assert store.map[filename] == payload.encode("utf-8")
+
+    root = zarr.group(store=store.map)
+    data = root.require_dataset("MODEL_DATA", shape=1000, dtype=np.complex128)
+
+    print(store.ls())
