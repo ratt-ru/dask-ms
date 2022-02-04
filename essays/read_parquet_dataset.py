8a3d4bbb2121c8084b57c81b5012e83911a71262
Simon Perkins <simon.perkins@gmail.com>
2022-02-04 12:21:45 +0200

    Pushdown predicates and Tensor reading

--- essays/read_parquet_dataset.py
+++ essays/read_parquet_dataset.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 from pathlib import Path
 from pprint import pprint
 
@@ -10,6 +11,7 @@ import pyarrow.parquet as pq
 
 from daskms.patterns import LazyProxy
 import daskms.experimental.arrow.extension_types
+from daskms.experimental.arrow.arrow_schema import DASKMS_METADATA
 
 def create_parser():
     p = argparse.ArgumentParser()
@@ -17,22 +19,32 @@ def create_parser():
     p.add_argument("-c", "--column", default="TIME")
     return p
 
-def read_column(file_proxy, column):
-    chunks = file_proxy[0].read(columns=[column]).column(column).chunks
+def read_column(table_proxy, column):
+    arrow_col = table_proxy.item().column(column)
+    chunks = arrow_col.chunks
+
+    if not chunks:
+        return np.array([])
+
     assert len(chunks) == 1
     zero_copy = chunks[0].type not in (pa.string(), pa.bool_())
     return chunks[0].to_numpy(zero_copy_only=zero_copy)
 
 def column(path, column):
     fragments = list(sorted(path.rglob("*.parquet")))
-    proxies = np.asarray([LazyProxy(pq.ParquetFile, f) for f in fragments])
-    # NOTE: instantiates ParquetFile's in the graph construction process
+    proxies = np.asarray([LazyProxy(pq.read_table, f, filters=[
+        [("ANTENNA1", "<=", 3),
+         ("ANTENNA2", ">=", 5)]]) for f in fragments])
+    # NOTE: instantiates Tables's in the graph construction process
     # for purposes of reading metadata
-    rows = np.asarray([p.metadata.num_rows for p in proxies])
+    rows = np.asarray([p.num_rows for p in proxies])
 
     # Get the table schema from the first file, this should
     # be the same for all files
-    schema = proxies[0].metadata.schema.to_arrow_schema()
+    schema = proxies[0].schema
+    with open("schema.txt", "w") as f:
+        f.write(str(schema))
+
     fields = {n: schema.field(n) for n in schema.names}
 
     try:
@@ -40,20 +52,29 @@ def column(path, column):
     except KeyError:
         raise ValueError(f"Parquet dataset has no column {column}")
 
+    field_metadata = field.metadata[DASKMS_METADATA.encode()]
+    field_metadata = json.loads(field_metadata)
+    dims = tuple(field_metadata["dims"])
+    shape = (rows,) + field.type.shape
+
+    assert len(shape) == len(dims)
+    meta = np.empty((0,)*len(dims), field.type.to_pandas_dtype())
+    new_axes = {d: s for d, s in zip(dims[1:], shape[1:])}
 
     dask_proxies = da.from_array(proxies, chunks=1)
-    dask_rows = da.from_array(rows, chunks=1)
+    # dask_rows = da.from_array(rows, chunks=1)
 
-
-    data = da.blockwise(read_column, ("row",),
+    data = da.blockwise(read_column, dims,
                         dask_proxies, ("row",),
                         column, None,
+                        new_axes=new_axes,
                         adjust_chunks={"row": tuple(rows.tolist())},
-                        meta=np.empty((0,), dtype=field.type.to_pandas_dtype()))
+                        meta=meta)
 
     return data
 
 if __name__ == "__main__":
     args = create_parser().parse_args()
     data = column(Path(args.ms), args.column)
-    print(data, data.compute())
+    print(data, data.compute(scheduler="sync"))
+    None
