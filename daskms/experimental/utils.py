e0aafa864b67a24a87f4a217471b89121b005c4a
JSKenyon <jonathan.simon.kenyon@gmail.com>
2022-04-12 15:18:00 +0200

    Fix bug in coordinate selection.

--- daskms/experimental/utils.py
+++ daskms/experimental/utils.py
@@ -86,8 +86,8 @@ def select_vars_and_coords(dataset, columns):
         data_sel = column_set & data_var_names
         coord_sel = column_set & coord_names
 
-        for v in data_vars.values():
-            coord_sel = coord_sel.union(*v.coords.keys())
+        for dv in data_sel:
+            coord_sel = coord_sel.union(*data_vars[dv].coords.keys())
 
         ret_data_vars = {col: data_vars[col] for col in data_sel}
         ret_coords = {c: coords[c] for c in coord_sel}
