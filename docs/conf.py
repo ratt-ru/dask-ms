27d2499f73152fa381df91d542441f0a4d1e9480
Simon Perkins <simon.perkins@gmail.com>
2022-09-22 12:02:21 +0200

    [skip ci] Add sphinx_copybutton

--- docs/conf.py
+++ docs/conf.py
@@ -67,6 +67,7 @@ needs_sphinx = '5.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
+    "sphinx_copybutton",
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.doctest',
