7a2b62aacd71cc271079e776d9034534079db3f6
Simon Perkins <simon.perkins@gmail.com>
2022-09-22 11:58:10 +0200

    Set language

--- docs/conf.py
+++ docs/conf.py
@@ -107,12 +107,12 @@ release = daskms.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'default'
