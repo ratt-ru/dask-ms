fafb9a12701b610f25416f4cea974cccf65b564b
Simon Perkins <simon.perkins@gmail.com>
2022-09-22 10:53:41 +0200

    Set furo pygments to default

--- docs/conf.py
+++ docs/conf.py
@@ -114,6 +114,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'default'
+furo_pygments = 'default'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
