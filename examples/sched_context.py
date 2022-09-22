452e5e48e30430a80c711df0de112e3d61df5b6d
Simon Perkins <simon.perkins@gmail.com>
2022-09-22 15:09:49 +0200

    Apply other black changes

--- examples/sched_context.py
+++ examples/sched_context.py
@@ -4,39 +4,41 @@ from contextlib import contextmanager
 
 @contextmanager
 def scheduler_context(args):
-    """ Set the scheduler to use, based on the script arguments """
+    """Set the scheduler to use, based on the script arguments"""
 
     import dask
 
     sched_info = {}
 
     try:
-        if args.scheduler in ("mt", "thread", "threads",
-                              "threaded", "threading"):
+        if args.scheduler in ("mt", "thread", "threads", "threaded", "threading"):
             logging.info("Using multithreaded scheduler")
             dask.config.set(scheduler="threads")
             sched_info = {"type": "threaded"}
         elif args.scheduler in ("mp", "processes", "multiprocessing"):
-            raise ValueError("The Process Scheduler does not currently "
-                             "work with dask-ms")
+            raise ValueError(
+                "The Process Scheduler does not currently " "work with dask-ms"
+            )
             import dask.multiprocessing
+
             logging.info("Using multiprocessing scheduler")
             dask.config.set(scheduler="processes")
             sched_info = {"type": "multiprocessing"}
         else:
             import distributed
+
             local_cluster = None
 
             if args.scheduler == "local":
                 local_cluster = distributed.LocalCluster(processes=False)
                 address = local_cluster.scheduler_address
-            elif args.scheduler.startswith('tcp'):
+            elif args.scheduler.startswith("tcp"):
                 address = args.scheduler
             else:
                 import json
 
-                with open(args.scheduler, 'r') as f:
-                    address = json.load(f)['address']
+                with open(args.scheduler, "r") as f:
+                    address = json.load(f)["address"]
 
             logging.info(f"Using distributed scheduler with address '{address}'")
             client = distributed.Client(address)
@@ -45,7 +47,8 @@ def scheduler_context(args):
             sched_info = {
                 "type": "distributed",
                 "client": client,
-                "local_cluster": local_cluster}
+                "local_cluster": local_cluster,
+            }
 
         yield
     except Exception:
