a47f99c5d214eab51c1d72f9bcfa43ecceb399d7
Simon Perkins <simon.perkins@gmail.com>
2022-09-22 09:27:29 +0200

    Switch to furo theme

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ import daskms  # noqa
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary',
               'sphinx.ext.doctest',
               'sphinx.ext.viewcode', 'sphinx.ext.intersphinx',
-              'sphinx.ext.extlinks', 'sphinx_rtd_theme', 'numpydoc']
+              'sphinx.ext.extlinks', 'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -121,7 +121,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
