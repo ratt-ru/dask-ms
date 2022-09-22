33991fe485865aece6790e51b627e7418a9cc60c
Simon Perkins <simon.perkins@gmail.com>
2022-09-22 12:59:01 +0200

    [skip ci] Override pygments.css

--- docs/conf.py
+++ docs/conf.py
@@ -106,6 +106,7 @@ html_theme_options = {}
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['css/pygments.css']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -132,3 +133,4 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'xarray': ('https://xarray.pydata.org/en/stable', None),
 }
+
