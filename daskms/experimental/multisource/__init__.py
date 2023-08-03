3b50340b59b54cecffecf33809d042382f108e72
JSKenyon <jonathan.simon.kenyon@gmail.com>
2023-08-03 16:47:37 +0200

    WIP on proxy reads.

--- daskms/experimental/multisource/__init__.py
+++ daskms/experimental/multisource/__init__.py
@@ -1,5 +1,7 @@
-from daskms import xds_from_storage_ms
+from daskms import xds_from_storage_ms, xds_to_storage_table
+from daskms.fsspec_store import DaskMSStore
 from daskms.utils import requires
+from daskms.experimental.zarr import xds_to_zarr, xds_from_zarr
 
 from collections.abc import Iterable
 
@@ -30,3 +32,38 @@ def xds_from_merged_storage(stores, **kwargs):
     )
 
     return [xarray.merge(xdss) for xdss in zip(*lxdsl)]
+
+
+def xds_from_proxy(store, **kwargs):
+
+    xdsl = xds_from_storage_ms(store, **kwargs)
+
+    parent_urls = {xds.attrs.get("__dask_ms_parent_url__", None) for xds in xdsl}
+
+    assert len(parent_urls) == 1, (
+        "Proxy has more than one parent - this is not supported."
+    )
+
+    parent_url = parent_urls.pop()
+
+    if parent_url:
+
+        if not isinstance(parent_url, DaskMSStore):
+            store = DaskMSStore(parent_url)
+
+        xdsl_nested = xds_from_proxy(store, **kwargs)
+    else:
+        return [xdsl]
+
+    return [xdsl, *xdsl_nested]
+
+
+
+def xds_to_proxy(xds, store, parent, **kwargs):
+
+    if not isinstance(parent, DaskMSStore):
+        parent = DaskMSStore(parent)
+
+    xds = [x.assign_attrs({"__dask_ms_parent_url__": parent.url}) for x in xds]
+
+    return xds_to_zarr(xds, store, **kwargs)
