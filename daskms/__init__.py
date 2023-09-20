37e2c608ceb2d36e1b20edd9d3dfc8436e16b01b
Simon Perkins <simon.perkins@gmail.com>
2023-09-20 09:37:13 +0200

    Bump to 0.2.18

--- daskms/__init__.py
+++ daskms/__init__.py
@@ -4,7 +4,7 @@ import logging
 
 __author__ = """Simon Perkins"""
 __email__ = "sperkins@ska.ac.za"
-__version__ = "0.2.17"
+__version__ = "0.2.18"
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
