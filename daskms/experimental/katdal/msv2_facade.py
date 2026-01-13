86dd0649b696ab2920d47e1bc442fc9d8690708d
Simon Perkins <simon.perkins@gmail.com>
2026-01-13 15:10:25 +0200

    Add lists, WEIGHT and SIGMA

--- daskms/experimental/katdal/msv2_facade.py
+++ daskms/experimental/katdal/msv2_facade.py
@@ -241,6 +241,7 @@ class XArrayMSv2Facade:
                 primary_dims + ("chan", "corr"),
                 weights.dataset,
             ),
+            "WEIGHT": (primary_dims + ("corr",), da.mean(weights.dataset, axis=1)),
             # Estimated RMS noise per frequency channel
             # note this column is used when computing calibration weights
             # in CASA - WEIGHT_SPECTRUM may be modified based on the
@@ -250,6 +251,10 @@ class XArrayMSv2Facade:
             "SIGMA_SPECTRUM": (
                 primary_dims + ("chan", "corr"),
                 weights.dataset**-0.5,
+            ),
+            "SIGMA": (
+                primary_dims + ("corr",),
+                da.mean(weights.dataset**-0.5, axis=1),
             ),
         }
 
@@ -267,24 +272,32 @@ class XArrayMSv2Facade:
     def _antenna_xarray_factory(self):
         antennas = self._dataset.ants
         nant = len(antennas)
-        return xarray.Dataset(
-            {
-                "NAME": ("row", np.asarray([a.name for a in antennas], dtype=object)),
-                "STATION": (
-                    "row",
-                    np.asarray([a.name for a in antennas], dtype=object),
-                ),
-                "POSITION": (
-                    ("row", "xyz"),
-                    np.asarray([a.position_ecef for a in antennas]),
-                ),
-                "OFFSET": (("row", "xyz"), np.zeros((nant, 3))),
-                "DISH_DIAMETER": ("row", np.asarray([a.diameter for a in antennas])),
-                "MOUNT": ("row", np.array(["ALT-AZ"] * nant, dtype=object)),
-                "TYPE": ("row", np.array(["GROUND-BASED"] * nant, dtype=object)),
-                "FLAG_ROW": ("row", np.zeros(nant, dtype=np.int32)),
-            }
-        )
+        return [
+            xarray.Dataset(
+                {
+                    "NAME": (
+                        "row",
+                        np.asarray([a.name for a in antennas], dtype=object),
+                    ),
+                    "STATION": (
+                        "row",
+                        np.asarray([a.name for a in antennas], dtype=object),
+                    ),
+                    "POSITION": (
+                        ("row", "xyz"),
+                        np.asarray([a.position_ecef for a in antennas]),
+                    ),
+                    "OFFSET": (("row", "xyz"), np.zeros((nant, 3))),
+                    "DISH_DIAMETER": (
+                        "row",
+                        np.asarray([a.diameter for a in antennas]),
+                    ),
+                    "MOUNT": ("row", np.array(["ALT-AZ"] * nant, dtype=object)),
+                    "TYPE": ("row", np.array(["GROUND-BASED"] * nant, dtype=object)),
+                    "FLAG_ROW": ("row", np.zeros(nant, dtype=np.int32)),
+                }
+            )
+        ]
 
     def _spw_xarray_factory(self):
         def ref_freq(chan_freqs):
@@ -330,84 +343,103 @@ class XArrayMSv2Facade:
         # MeerKAT only has linear feeds, these map to
         # CASA ["XX", "XY", "YX", "YY"]
         pol_types = {"HH": 9, "HV": 10, "VH": 11, "VV": 12}
-        return xarray.Dataset(
-            {
-                "NUM_CORR": ("row", np.array([len(self._pols_to_use)], dtype=np.int32)),
-                "CORR_PRODUCT": (
-                    ("row", "corr", "corrprod_idx"),
-                    np.array(
-                        [[[pol_num[p[0]], pol_num[p[1]]] for p in self._pols_to_use]],
-                        dtype=np.int32,
+        return [
+            xarray.Dataset(
+                {
+                    "NUM_CORR": (
+                        "row",
+                        np.array([len(self._pols_to_use)], dtype=np.int32),
                     ),
-                ),
-                "CORR_TYPE": (
-                    ("row", "corr"),
-                    np.asarray(
-                        [[pol_types[p] for p in self._pols_to_use]], dtype=np.int32
+                    "CORR_PRODUCT": (
+                        ("row", "corr", "corrprod_idx"),
+                        np.array(
+                            [
+                                [
+                                    [pol_num[p[0]], pol_num[p[1]]]
+                                    for p in self._pols_to_use
+                                ]
+                            ],
+                            dtype=np.int32,
+                        ),
                     ),
-                ),
-                "FLAG_ROW": ("row", np.zeros(1, dtype=np.int32)),
-            }
-        )
+                    "CORR_TYPE": (
+                        ("row", "corr"),
+                        np.asarray(
+                            [[pol_types[p] for p in self._pols_to_use]], dtype=np.int32
+                        ),
+                    ),
+                    "FLAG_ROW": ("row", np.zeros(1, dtype=np.int32)),
+                }
+            )
+        ]
 
     def _ddid_xarray_factory(self):
-        return xarray.Dataset(
-            {
-                "SPECTRAL_WINDOW_ID": ("row", np.zeros(1, dtype=np.int32)),
-                "POLARIZATION_ID": ("row", np.zeros(1, dtype=np.int32)),
-                "FLAG_ROW": ("row", np.zeros(1, dtype=np.int32)),
-            }
-        )
+        return [
+            xarray.Dataset(
+                {
+                    "SPECTRAL_WINDOW_ID": ("row", np.zeros(1, dtype=np.int32)),
+                    "POLARIZATION_ID": ("row", np.zeros(1, dtype=np.int32)),
+                    "FLAG_ROW": ("row", np.zeros(1, dtype=np.int32)),
+                }
+            )
+        ]
 
     def _feed_xarray_factory(self):
         nfeeds = len(self._dataset.ants)
         NRECEPTORS = 2
 
-        return xarray.Dataset(
-            {
-                # ID of antenna in this array (integer)
-                "ANTENNA_ID": ("row", np.arange(nfeeds, dtype=np.int32)),
-                # Id for BEAM model (integer)
-                "BEAM_ID": ("row", np.ones(nfeeds, dtype=np.int32)),
-                # Beam position offset (on sky but in antenna reference frame): (double, 2-dim)
-                "BEAM_OFFSET": (
-                    ("row", "receptors", "radec"),
-                    np.zeros((nfeeds, 2, 2), dtype=np.float64),
-                ),
-                # Feed id (integer)
-                "FEED_ID": ("row", np.zeros(nfeeds, dtype=np.int32)),
-                # Interval for which this set of parameters is accurate (double)
-                "INTERVAL": ("row", np.zeros(nfeeds, dtype=np.float64)),
-                # Number of receptors on this feed (probably 1 or 2) (integer)
-                "NUM_RECEPTORS": ("row", np.full(nfeeds, NRECEPTORS, dtype=np.int32)),
-                # Type of polarisation to which a given RECEPTOR responds (string, 1-dim)
-                "POLARIZATION_TYPE": (
-                    ("row", "receptors"),
-                    np.array([["X", "Y"]] * nfeeds, dtype=object),
-                ),
-                # D-matrix i.e. leakage between two receptors (complex, 2-dim)
-                "POL_RESPONSE": (
-                    ("row", "receptors", "receptors-2"),
-                    np.array([np.eye(2, dtype=np.complex64) for _ in range(nfeeds)]),
-                ),
-                # Position of feed relative to feed reference position (double, 1-dim, shape=(3,))
-                "POSITION": (("row", "xyz"), np.zeros((nfeeds, 3), np.float64)),
-                # The reference angle for polarisation (double, 1-dim). A parallactic angle of
-                # 0 means that V is aligned to x (celestial North), but we are mapping H to x
-                # so we have to correct with a -90 degree rotation.
-                "RECEPTOR_ANGLE": (
-                    ("row", "receptors"),
-                    np.full((nfeeds, NRECEPTORS), -np.pi / 2, dtype=np.float64),
-                ),
-                # ID for this spectral window setup (integer)
-                "SPECTRAL_WINDOW_ID": ("row", np.full(nfeeds, -1, dtype=np.int32)),
-                # Midpoint of time for which this set of parameters is accurate (double)
-                "TIME": ("row", np.zeros(nfeeds, dtype=np.float64)),
-            }
-        )
+        return [
+            xarray.Dataset(
+                {
+                    # ID of antenna in this array (integer)
+                    "ANTENNA_ID": ("row", np.arange(nfeeds, dtype=np.int32)),
+                    # Id for BEAM model (integer)
+                    "BEAM_ID": ("row", np.ones(nfeeds, dtype=np.int32)),
+                    # Beam position offset (on sky but in antenna reference frame): (double, 2-dim)
+                    "BEAM_OFFSET": (
+                        ("row", "receptors", "radec"),
+                        np.zeros((nfeeds, 2, 2), dtype=np.float64),
+                    ),
+                    # Feed id (integer)
+                    "FEED_ID": ("row", np.zeros(nfeeds, dtype=np.int32)),
+                    # Interval for which this set of parameters is accurate (double)
+                    "INTERVAL": ("row", np.zeros(nfeeds, dtype=np.float64)),
+                    # Number of receptors on this feed (probably 1 or 2) (integer)
+                    "NUM_RECEPTORS": (
+                        "row",
+                        np.full(nfeeds, NRECEPTORS, dtype=np.int32),
+                    ),
+                    # Type of polarisation to which a given RECEPTOR responds (string, 1-dim)
+                    "POLARIZATION_TYPE": (
+                        ("row", "receptors"),
+                        np.array([["X", "Y"]] * nfeeds, dtype=object),
+                    ),
+                    # D-matrix i.e. leakage between two receptors (complex, 2-dim)
+                    "POL_RESPONSE": (
+                        ("row", "receptors", "receptors-2"),
+                        np.array(
+                            [np.eye(2, dtype=np.complex64) for _ in range(nfeeds)]
+                        ),
+                    ),
+                    # Position of feed relative to feed reference position (double, 1-dim, shape=(3,))
+                    "POSITION": (("row", "xyz"), np.zeros((nfeeds, 3), np.float64)),
+                    # The reference angle for polarisation (double, 1-dim). A parallactic angle of
+                    # 0 means that V is aligned to x (celestial North), but we are mapping H to x
+                    # so we have to correct with a -90 degree rotation.
+                    "RECEPTOR_ANGLE": (
+                        ("row", "receptors"),
+                        np.full((nfeeds, NRECEPTORS), -np.pi / 2, dtype=np.float64),
+                    ),
+                    # ID for this spectral window setup (integer)
+                    "SPECTRAL_WINDOW_ID": ("row", np.full(nfeeds, -1, dtype=np.int32)),
+                    # Midpoint of time for which this set of parameters is accurate (double)
+                    "TIME": ("row", np.zeros(nfeeds, dtype=np.float64)),
+                }
+            )
+        ]
 
     def _field_xarray_factory(self, field_data):
-        fields = [
+        return [
             xarray.Dataset(
                 {
                     "NAME": ("row", np.array([target.name], object)),
@@ -433,64 +465,71 @@ class XArrayMSv2Facade:
             for field_id, time, target, radec in field_data.values()
         ]
 
-        return xarray.concat(fields, dim="row")
-
     def _source_xarray_factory(self, field_data):
         field_ids, times, targets, radecs = zip(*(field_data.values()))
         times = np.array(times, dtype=np.float64)
         nfields = len(field_ids)
-        return xarray.Dataset(
-            {
-                "NAME": ("row", np.array([t.name for t in targets], dtype=object)),
-                "SOURCE_ID": ("row", np.array(field_ids, dtype=np.int32)),
-                "PROPER_MOTION": (
-                    ("row", "radec-per-sec"),
-                    np.zeros((nfields, 2), dtype=np.float32),
-                ),
-                "CALIBRATION_GROUP": ("row", np.full(nfields, -1, dtype=np.int32)),
-                "DIRECTION": (("row", "radec"), np.array(radecs)),
-                "TIME": ("row", times),
-                "NUM_LINES": ("row", np.ones(nfields, dtype=np.int32)),
-                "REST_FREQUENCY": (
-                    ("row", "lines"),
-                    np.zeros((nfields, 1), dtype=np.float64),
-                ),
-            }
-        )
+        return [
+            xarray.Dataset(
+                {
+                    "NAME": ("row", np.array([t.name for t in targets], dtype=object)),
+                    "SOURCE_ID": ("row", np.array(field_ids, dtype=np.int32)),
+                    "PROPER_MOTION": (
+                        ("row", "radec-per-sec"),
+                        np.zeros((nfields, 2), dtype=np.float32),
+                    ),
+                    "CALIBRATION_GROUP": ("row", np.full(nfields, -1, dtype=np.int32)),
+                    "DIRECTION": (("row", "radec"), np.array(radecs)),
+                    "TIME": ("row", times),
+                    "NUM_LINES": ("row", np.ones(nfields, dtype=np.int32)),
+                    "REST_FREQUENCY": (
+                        ("row", "lines"),
+                        np.zeros((nfields, 1), dtype=np.float64),
+                    ),
+                }
+            )
+        ]
 
     def _state_xarray_factory(self, state_modes):
         state_ids, modes = zip(*sorted((i, m) for m, i in state_modes.items()))
         nstates = len(state_ids)
-        return xarray.Dataset(
-            {
-                "SIG": np.ones(nstates, dtype=np.uint8),
-                "REF": np.zeros(nstates, dtype=np.uint8),
-                "CAL": np.zeros(nstates, dtype=np.float64),
-                "LOAD": np.zeros(nstates, dtype=np.float64),
-                "SUB_SCAN": np.zeros(nstates, dtype=np.int32),
-                "OBS_MODE": np.array(modes, dtype=object),
-                "FLAG_ROW": np.zeros(nstates, dtype=np.int32),
-            }
-        )
+        return [
+            xarray.Dataset(
+                {
+                    "SIG": np.ones(nstates, dtype=np.uint8),
+                    "REF": np.zeros(nstates, dtype=np.uint8),
+                    "CAL": np.zeros(nstates, dtype=np.float64),
+                    "LOAD": np.zeros(nstates, dtype=np.float64),
+                    "SUB_SCAN": np.zeros(nstates, dtype=np.int32),
+                    "OBS_MODE": np.array(modes, dtype=object),
+                    "FLAG_ROW": np.zeros(nstates, dtype=np.int32),
+                }
+            )
+        ]
 
     def _observation_xarray_factory(self):
         ds = self._dataset
         start, end = [to_mjds(t) for t in [ds.start_time, ds.end_time]]
-        return xarray.Dataset(
-            {
-                "OBSERVER": ("row", np.array([ds.observer], dtype=object)),
-                "PROJECT": ("row", np.array([ds.experiment_id], dtype=object)),
-                "LOG": (("row", "extra"), np.array([["unavailable"]], dtype=object)),
-                "SCHEDULE": (
-                    ("row", "extra"),
-                    np.array([["unavailable"]], dtype=object),
-                ),
-                "SCHEDULE_TYPE": ("row", np.array(["unknown"], dtype=object)),
-                "TELESCOPE_NAME": ("row", np.array(["MeerKAT"], dtype=object)),
-                "TIME_RANGE": (("row", "extent"), np.array([[start, end]])),
-                "FLAG_ROW": ("row", np.zeros(1, np.uint8)),
-            }
-        )
+        return [
+            xarray.Dataset(
+                {
+                    "OBSERVER": ("row", np.array([ds.observer], dtype=object)),
+                    "PROJECT": ("row", np.array([ds.experiment_id], dtype=object)),
+                    "LOG": (
+                        ("row", "extra"),
+                        np.array([["unavailable"]], dtype=object),
+                    ),
+                    "SCHEDULE": (
+                        ("row", "extra"),
+                        np.array([["unavailable"]], dtype=object),
+                    ),
+                    "SCHEDULE_TYPE": ("row", np.array(["unknown"], dtype=object)),
+                    "TELESCOPE_NAME": ("row", np.array(["MeerKAT"], dtype=object)),
+                    "TIME_RANGE": (("row", "extent"), np.array([[start, end]])),
+                    "FLAG_ROW": ("row", np.zeros(1, np.uint8)),
+                }
+            )
+        ]
 
     def xarray_datasets(self):
         """Generates partitions of the main MSv2 table, as well as the subtables.
